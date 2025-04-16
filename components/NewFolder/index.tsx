ce284bdef5817d3f5234a481f49bbb2c7452251f
chukwuebuka <emmanuel.henry71@gmail.com>
2025-04-16 12:48:58 +0100

    feat:: made neccessary intallment

--- components/NewFolder/index.tsx
+++ components/NewFolder/index.tsx
@@ -1,11 +1,11 @@
-// import React from 'react'
+import React from "react";
 
-function index() {
+function Index(): React.ReactElement {
   return (
     <div>
       <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default index
+export default Index;
