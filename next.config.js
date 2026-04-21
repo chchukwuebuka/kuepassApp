621ddb7155cf0f8866519fdd9104c6d7ffe5f488
chukwuebuka <emmanuel.henry71@gmail.com>
2026-04-21 14:47:28 +0100

    yes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 const nextConfig = {
+  output: 'standalone',
   // Your other Next.js configurations can go here
   reactStrictMode: true, // Example other config
 
