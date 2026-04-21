93a1205893e5b555c0f43cc781199d88c94997b5
chukwuebuka <emmanuel.henry71@gmail.com>
2026-04-21 14:29:10 +0100

    Restore dashboard layout and properly delete static layout.tsx hacks

--- app/complete-registration/[token]/layout.tsx
+++
@@ -1,9 +0,0 @@
-export const dynamicParams = false;
-
-export function generateStaticParams() {
-  return [];
-}
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Navbar from "@/components/navbar";
+import { Roboto } from "next/font/google";
+import styles from "./styles.module.css";
+import type { ReactNode } from "react";
+import React from "react";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+
+export default function DashboardLayout({ children }: { children: ReactNode }) {
+  return (
+    <>
+    <div className={styles.dashboardLayout}>
+
+      <Navbar />
+    </div>
+      <div className={styles.dashboardLayout}>
+        <div className={`${styles.container} ${roboto.className}`}>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
